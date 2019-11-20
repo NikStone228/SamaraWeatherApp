f8ffcafc663b11463f19f489f1a1ab363fbdbf50
Nik <nikitosvanilla@gmail.com>
2019-11-20 21:22:55 +0400

    Initial Commit

---
+++ SamaraWeatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SamaraWeatherApp
+//
+//  Created by Nikita on 20/11/2019.
+//  Copyright © 2019 Nikita. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
